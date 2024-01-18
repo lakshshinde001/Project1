5b5fa95070b66d6576822f69319cacddb2b5549d
lakshshinde001 <laxmanshinde3105@gmail.com>
2024-01-18 19:51:54 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const panels = document.querySelectorAll('.panel');
+// console.log(panels);
+panels.forEach(panel =>{
+    panel.addEventListener('click', ()=>{
+        removeClass()
+        panel.classList.add('active');
+    })
+})
+function removeClass(){
+    panels.forEach(panel =>{
+        panel.classList.remove('active');
+    })
+}
